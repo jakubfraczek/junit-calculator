3b185ef0617666e688d7869b5e2880b1ff764f0c
kurs <kurs@alx>
2016-07-30 11:09:52 +0200

    Added tests for class Calculator

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -4,26 +4,67 @@ import main.Calculator;
 
 import org.junit.Before;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
-	
+
 	private Calculator classUnderTest;
-	
+
 	@Before
 	public void setUp() {
-		System.out.println("Metoda zostanie wykonana przed kazdym testem");
 		classUnderTest = new Calculator();
 	}
-		
-	@After
-	public void tearDown() {
-		System.out.println("Metoda zostanie wykonana po kazdym tescie");
-	}
+
 	
 	@Test
-	public void test1() throws Exception {
-		System.out.println("Metoda testowa");
+	public void testSum() throws Exception {
+		double z1 = 1.6;
+		double z2 = 1.8;
+
+		double wynik = classUnderTest.sum(z1, z2);
+
+		Assert.assertEquals(3.4, wynik, 0.1);
+	}
+
+	@Test
+	public void testSubstruct() throws Exception {
+		double z1 = 5.6;
+		double z2 = 3.3;
+
+		double wynik = classUnderTest.subtract(z1, z2);
+
+		Assert.assertEquals(2.3, wynik, 0.00000001);
+	}
+
+	@Test
+	public void testMultiply() throws Exception {
+		double z1 = 2.3;
+		double z2 = 3.7;
+
+		double wynik = classUnderTest.multiply(z1, z2);
+
+		Assert.assertEquals(8.51, wynik, 0.00000001);
+	}
+
+	@Test
+	public void testDivide() throws Exception {
+		double z1 = 8.51;
+		double z2 = 2.3;
+
+		double wynik = classUnderTest.divide(z1, z2);
+
+		Assert.assertEquals(3.7, wynik, 0.00000007);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testDivideEx() throws Exception {
+		double z1 = 2.5;
+		double z2 = 0;
+		
+		double wynik = classUnderTest.divide(z1, z2);
+		
+		
 	}
 
 }
